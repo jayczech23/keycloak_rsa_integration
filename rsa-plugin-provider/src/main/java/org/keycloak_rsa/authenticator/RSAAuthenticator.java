8ac1b817b9831a7d3b695b739adae38f0df9bc15
Jordan Cech <cech.jordan@gmail.com>
2023-11-30 22:18:25 +0000

    Updated deprecated JAVAX libraries to up-to-date jakarta EE libs.

--- rsa-plugin-provider/src/main/java/org/keycloak_rsa/authenticator/RSAAuthenticator.java
+++ rsa-plugin-provider/src/main/java/org/keycloak_rsa/authenticator/RSAAuthenticator.java
@@ -9,11 +9,12 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
+
 import java.util.*;
 
 import static org.rsa.authenticator.Const.*;
